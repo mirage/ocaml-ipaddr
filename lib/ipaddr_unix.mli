efdf773f47e514fb494c376f5644c57d4c2f864c
David Sheets <sheets@alum.mit.edu>
2014-01-26 16:26:00 +0000

    Standardize and correct Ipaddr_unix docs

--- lib/ipaddr_unix.mli
+++ lib/ipaddr_unix.mli
@@ -17,45 +17,43 @@
 
 (** Convert to and from [Unix] to [Ipaddr] representations *)
 
-(** [to_inet_addr ip] converts an IPv4 or IPv6 address into an
-    equivalent [Unix.inet_addr] *)
+(** [to_inet_addr ip] is the {! Unix.inet_addr} equivalent of the
+    IPv4 or IPv6 address [ip]. *)
 val to_inet_addr : Ipaddr.t -> Unix.inet_addr
 
-(** [of_inet_addr ip] converts a [Unix.inet_addr] into an
-    equivalent IPv4 or IPv6 address. *)
+(** [of_inet_addr ip] is the {! Ipaddr.t} equivalent of the
+    {! Unix.inet_addr} [ip]. *)
 val of_inet_addr : Unix.inet_addr -> Ipaddr.t
 
 module V4 : sig
 
-  (** [to_inet_addr ip] converts an IPv4 address into an
-    equivalent [Unix.inet_addr] *)
+  (** [to_inet_addr ip] is the {! Unix.inet_addr} equivalent of the
+      IPv4 address [ip]. *)
   val to_inet_addr : Ipaddr.V4.t -> Unix.inet_addr
 
-  (** [of_inet_addr ip] converts a {! Unix.inet_addr} into an
-      equivalent IPv4 address.  If the {! Unix.inet_addr} is
-      not a valid IPv4 address, then [None] will be returned. *)
-  val of_inet_addr : Unix.inet_addr -> Ipaddr.V4.t option
-
-  (** [of_inet_addr_exn ip] converts a {!Unix.inet_addr} into an
-      equivalent IPv4 address.  If the {!Unix.inet_addr} is
-      not a valid IPv4 address, then {!Failure} will be raised. *)
+  (** [of_inet_addr_exn ip] is the {! Ipaddr.t} equivalent of the
+      {!Unix.inet_addr} [ip] IPv4 address. Raises {! Ipaddr.Parse_error} if
+      [ip] is not a valid representation of an IPv4 address. *)
   val of_inet_addr_exn : Unix.inet_addr -> Ipaddr.V4.t
+
+  (** Same as [of_inet_addr_exn] but returns an option type instead of raising
+      an exception. *)
+  val of_inet_addr : Unix.inet_addr -> Ipaddr.V4.t option
 end
 
 module V6 : sig
 
-  (** [to_inet_addr ip] converts an IPv6 address into an
-    equivalent [Unix.inet_addr] *)
+  (** [to_inet_addr ip] is the {! Unix.inet_addr} equivalent of the
+      IPv6 address [ip]. *)
   val to_inet_addr : Ipaddr.V6.t -> Unix.inet_addr
 
-  (** [of_inet_addr ip] converts a {!Unix.inet_addr} into an
-      equivalent IPv6 address.  If the {!Unix.inet_addr} is
-      not a valid IPv6 address, then [None] will be returned. *)
-  val of_inet_addr : Unix.inet_addr -> Ipaddr.V6.t option
 
-  (** [of_inet_addr_exn ip] converts a {!Unix.inet_addr} into an
-      equivalent IPv6 address.  If the {!Unix.inet_addr} is
-      not a valid IPv6 address, then {!Failure} will be raised. *)
+  (** [of_inet_addr_exn ip] is the {! Ipaddr.t} equivalent of the
+      {!Unix.inet_addr} [ip] IPv6 address. Raises {! Ipaddr.Parse_error} if
+      [ip] is not a valid representation of an IPv6 address. *)
   val of_inet_addr_exn : Unix.inet_addr -> Ipaddr.V6.t
 
+  (** Same as [of_inet_addr_exn] but returns an option type instead of raising
+      an exception. *)
+  val of_inet_addr : Unix.inet_addr -> Ipaddr.V6.t option
 end
